68844a4991782d5892aadfd3a2d1a8bbfd64517a
IbnuShemsudin <musteshemsu@gmail.com>
2025-11-08 03:38:24 -0800

    setup the environment and create component directory

---
+++ portfoliowebsite/src/components/Navbar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import './Navbar.css'
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <h1>My Portfolio</h1>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  )
+}
+
+export default Navbar
