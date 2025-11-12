de397813887b09037bbeed9ac33a2758ba34b738
Ibnushemsudin <musteshemsu@gmail.com>
2025-11-12 12:26:52 -0800

    Add Navbar

--- portfoliowebsite/src/components/Navbar/Navbar.jsx
+++ portfoliowebsite/src/components/Navbar/Navbar.jsx
@@ -4,15 +4,15 @@ import logo from '../../assets/logo.png'
 const Navbar = () => {
   return (
     <div className="navbar">
-      <img src={logo} alt="" />
-      <ul>
+      <img src={logo} alt="logo" />
+      <ul className="nav-links">
         <li>Home</li>
         <li>About</li>
         <li>Projects</li>
         <li>Skills</li>
         <li>Contact</li>
       </ul>
-      <div>Connect with me</div>
+      <div className="connect">Connect with me</div>
     </div>
   )
 }
